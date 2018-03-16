dc949f83b359d2266d80a5966b184bd91ddefcaf
Garbes <brugarbes@gmail.com>
2018-03-16 15:01:51 -0300

    Applying a for command for different RUVSeq  K's
    Finally got to use an for command for  generating the RUVSeq PCA graphs (it's a more automatic and elegant command)

--- NormalizationStepsNeurons.R
+++ NormalizationStepsNeurons.R
@@ -28,7 +28,6 @@ counts=read.table("countdata_20M_neurons.txt", header=TRUE)
 sampleData=read.delim("samplesheet_neurons.txt", sep="\t", header=TRUE)
 HKgenes_full <- read.csv ("HK_full_gene_list_ensembl_biomart.csv")
 
-
 #first filtering
 
 filtered <- which(rowSums(geneFPKM > 1) >= 12)
@@ -46,6 +45,29 @@ geneFPKM50 <- geneFPKM [,c(1,2,5:7,9:15,17:23)]
 filtered <- which(rowSums(geneFPKM50 > 1) >= 9)
 FPKM <- geneFPKM50[filtered,]
 countData50 =subset (countData50, rownames(countData50) %in% rownames(FPKM))
+
+#first differential expression analysis
+#differential expression with DESeq2
+#we generated a gene list which is known to be no differentially expressed (pvalue>=0.4)
+#so we can built up a housekeeping gene list for RUVseq analysis
+
+dds <- DESeqDataSetFromMatrix(countData = countData50, colData = sampleData50, design = ~ condition)
+
+#set which group is the reference group
+dds$condition <- relevel(dds$condition, ref="C")
+ddsdeseq <- DESeq(dds)
+res <- results(ddsdeseq)
+#you can check the number of outliers and lowcount genes using the function summary
+summary(res)
+res <- as.data.frame(res)
+nDEGs_deseq50 <- subset (res, res$pvalue>=0.4)
+nDEGs_deseq50 <- rownames(nDEGs_deseq50)
+
+#list of housekeeping genes used for the RUVseq normalization analysis
+HKgenes50 <- intersect(HKgenes_full$Ensembl_biomart, rownames(countData50))
+nDEGs_HKgenes50 <- intersect(nDEGs_deseq50,HKgenes50)
+m=match(colnames(countData50), sampleData50$label)
+
 
 #RUV
 
@@ -71,51 +93,33 @@ pheatmap(sampleDistMatrix,
 dev.off()
 
 
-#first differential expression analysis
-#differential expression with DESeq2
-#we generated a gene list which is known to be no differentially expressed (pvalue>=0.8)
-#so we can built up a housekeeping gene list for RUVseq analysis
-
-dds <- DESeqDataSetFromMatrix(countData = countData50, colData = sampleData50, design = ~ condition)
-
-#set which group is the reference group
-dds$condition <- relevel(dds$condition, ref="C")
-ddsdeseq <- DESeq(dds)
-res <- results(ddsdeseq)
-#you can check the number of outliers and lowcount genes using the function summary
-summary(res)
-res <- as.data.frame(res)
-nDEGs_deseq50 <- subset (res, res$pvalue>=0.8)
-nDEGs_deseq50 <- rownames(nDEGs_deseq50)
-
-#list of housekeeping genes used for the RUVseq normalization analysis
-HKgenes50 <- intersect(HKgenes_full$Ensembl_biomart, rownames(countData50))
-nDEGs_HKgenes50 <- intersect(nDEGs_deseq50,HKgenes50)
-m=match(colnames(countData50), sampleData50$label)
-
-
 #RUVseq analysis
 
 controlGenes <- nDEGs_HKgenes50
-pdf("contrGenes_nDEGs_HKgenes_neurons50.pdf")
+pdf("TESTANDO_LACO_contrGenes_nDEGs_HKgenes_neurons50.pdf")
 
-RUV4 <- RUVg(as.matrix(countData50), controlGenes, k=4)
-N4 <- RUV4$normalizedCounts
+for( i in 1:4){
 
-boxplot(log2(N4))
-pca=princomp(cor(N4, method="s"))
-plot(pca$loadings[,1], pca$loadings[,2],xlab="PC1", ylab="PC2", main="PCA,RUV Normalisation k4, neur proportion", pch=20, col=labels2colors(sampleData50$Neur_prop_group[m]))
-dds <- DESeqDataSetFromMatrix(countData = N4, colData = sampleData50, design = ~ condition)
-vsd <- varianceStabilizingTransformation(dds)
-plotPCA(vsd, intgroup=c("Neur_prop_group"))
-sampleDists <- dist(t(assay(vsd)))
-sampleDistMatrix <- as.matrix(sampleDists)
-rownames(sampleDistMatrix) <- paste(colnames(countData50))
-colnames(sampleDistMatrix) <- NULL
-colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
-pheatmap(sampleDistMatrix,
+ RUV <- RUVg(as.matrix(countData50), controlGenes, k=i)
+ N <- RUV$normalizedCounts
+
+ boxplot(log2(N))
+ title("Boxplot Neuron Samples, 1<= K <=4")
+ pca=princomp(cor(N, method="s"))
+ plot(pca$loadings[,1], pca$loadings[,2],xlab="PC1", ylab="PC2", main="RUV Normalization PCA grph, neuron proportions", pch=20, col=labels2colors(sampleData50$Neur_prop_group[m]))
+ dds <- DESeqDataSetFromMatrix(countData = N, colData = sampleData50, design = ~ condition)
+ vsd <- varianceStabilizingTransformation(dds)
+ plotPCA(vsd, intgroup=c("Neur_prop_group"))
+ sampleDists <- dist(t(assay(vsd)))
+ sampleDistMatrix <- as.matrix(sampleDists)
+ rownames(sampleDistMatrix) <- paste(colnames(countData50))
+ colnames(sampleDistMatrix) <- NULL
+ colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
+ pheatmap(sampleDistMatrix,
          clustering_distance_rows=sampleDists,
          clustering_distance_cols=sampleDists,
          col=colors)
+}
+dev.off()
 
 #RODAR WGCNA AQUI POIS ELE TAMBEM FOI USADO NA ESCOLHA DO MELHOR CONJUNTO DE DADOS
